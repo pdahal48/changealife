0799cf335a1d939b65febed5309c494e26ba0267
Prem Dahal <premdahal@prems-mbp.lan>
2021-08-26 16:10:24 -0400

    screen size configurations

--- Frontend/cal/src/SuccessStories.js
+++ Frontend/cal/src/SuccessStories.js
@@ -5,11 +5,11 @@ const SuccessStories = ({ src, story }) => {
     return (
         <div>
             <div>
-            <Row className="container-fluid justify-content-center  mt-3">
-            <Col className="success-image col-xs-12 col-sm-12 col-xl-5 mb-5">
+            <Row className="container-fluid mt-3 justify-content-center">
+            <Col className="col-12 col-md-5 ol-xl-3 mb-5">
                 <img style={{height: "100%", width:"100%"}} src={src} alt={'success'}/>
             </Col>
-            <Col className="col-xs-12 col-sm-12  col-xl-3">
+            <Col className="col-12 col-md-5 col-xl-3">
                 <p className = "display-5 mb-3" style={{fontSize:"1.5rem", marginLeft:"10px"}}>
                     {story}
                 </p>
